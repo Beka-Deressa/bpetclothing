789d7f0b6c259bc21fc5654fed2c39ffbae86dc3
Tesfu <tesfuhabtu21@gmail.com>
2023-06-08 12:34:27 -0400

    Display the cart-item in form using sessionStorage

--- script.js
+++ script.js
@@ -1,78 +1,14 @@
 
-
-function handleFormSubmit(event) {
+document.getElementById("login-btn").addEventListener("click", function(event) {
     event.preventDefault();
-
-    const form = document.getElementById("itemForm");
-    const itemname = document.getElementById("name").value;
-    const itemImageurl = document.getElementById("url").value;
-    const itemDescription = document.getElementById("description").value;
-    const itemUnitPrice = document.getElementById("price").value;
-    const itemCategory = document.getElementById("category").value;
-
-    const card = document.createElement("div");
-    card.className = "card1";
-
-
-    const title = document.createElement("h1");
-    title.className = "title";
-    title.textContent = itemname;
-
-    const image = document.createElement("img");
-    image.src = itemImageurl;
-
-    const description = document.createElement("p");
-    description.textContent = itemDescription;
-
-    const category = document.createElement("p");
-    category.className = "category";
-    category.textContent = itemCategory;
-
-    const price = document.createElement("p");
-    price.className = "price";
-    price.textContent = "$" + itemUnitPrice;
-
-    card.appendChild(title);
-    card.appendChild(image);
-    card.appendChild(description);
-    card.appendChild(category);
-    card.appendChild(price);
-
-    document.getElementById("cards").appendChild(card);
-
-    form.reset();
-}
-
-window.addEventListener("DOMContentLoaded", function () {
-    const sellItemButton = document.getElementById("sellitem");
-    const form = document.getElementById("itemForm");
-    form.addEventListener("submit", handleFormSubmit);
-    sellItemButton.addEventListener("click", function () {
-
-        if (form.style.display === "none") {
-            form.style.display = "block";
-        } else {
-            form.style.display = "none";
-        }
-
-    })
-
-});
-
-window.addEventListener("DOMContentLoaded", function () {
-    const loginButton = document.getElementById("login-btn");
     const form = document.getElementById("login");
+    
+    if (form.style.display === "none") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
+
     form.addEventListener("submit", handleFormSubmit);
-    loginButton.addEventListener("click", function () {
-
-        if (form.style.display === "none") {
-            form.style.display = "block";
-        } else {
-            form.style.display = "none";
-        }
-
-    })
-
-});
-
+})
 
